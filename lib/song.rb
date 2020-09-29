2533167be6822c5c7cacb9a09bd3cd4652f820c6
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-29 20:28:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,4 +23,20 @@ class Song
   def self.artists
     @@artists.uniq
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count        
+  end
+
+  def self.artist_count
+
+  end
 end
