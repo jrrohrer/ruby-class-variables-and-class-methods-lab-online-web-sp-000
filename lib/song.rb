6c9854cebf08df0cfa60942b49fe09686118af2e
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-29 19:48:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,20 @@
 class Song
   @@count = 0
+  @@genres = []
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres
   end
 end
